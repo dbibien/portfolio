4f3637417ded62cd8f21083d83972f5af62512e7
Bibi <dbibien@gmail.com>
2024-10-01 00:21:13 -0400

    add: avatar

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,39 @@
+import Photo from "@/components/Photo"
 import SocialLinks from "@/components/SocialLinks"
 import { Button } from "@/components/ui/button"
 import { Download } from "lucide-react"
 
 const Home = () => {
   return (
-    <div className="pt-8">
-      <p className="text-center text-lg text-white tracking-wide underline lg:text-left">Full Stack Software Developer</p>
+    <div className="">
 
-      <h1 className="text-center text-white font-semibold text-4xl lg:text-left">
-        Hello, I am <br /> <span className="text-orange-400">Bibien Dauphin</span>
-      </h1>
+      <div className="lg:flex lg:flex-row">
+        <div className="lg:order-2">
+          <Photo />
+        </div>
 
-      <p className="text-center text-white text-lg pt-4 lg:text-left">
-        I specialize in creating scalable and efficient web applications that deliver seamless user experiences
-      </p>
+        <div className="mt-4 lg:order-1">
+          <p className="text-center text-lg text-white tracking-wide underline lg:text-left">Full Stack Software Developer</p>
 
-      <div className="mt-4 flex flex-col gap-4 items-center lg:flex lg:flex-row lg:items-center lg:gap-2">
-        <Button
-          variant="outline"
-          className="text-lg flex items-center gap-2  w-52 h-12"
-        >
-          Download Resume
-          <Download size="60" />
-        </Button>
+          <h1 className="text-center text-white font-semibold text-4xl lg:text-left">
+            Hello, I am <br /> <span className="text-orange-400">Bibien Dauphin</span>
+          </h1>
 
-        <SocialLinks />
+          <p className="text-center text-white text-lg pt-4 lg:text-left">
+            I specialize in creating scalable and efficient web applications that deliver seamless user experiences
+          </p>
+
+          <div className="mt-4 flex flex-col gap-4 items-center lg:flex lg:flex-row lg:items-center lg:gap-2">
+            <Button
+              variant="outline"
+              className="text-lg flex items-center gap-2  w-52 h-12"
+            >
+              Download Resume
+              <Download size="60" />
+            </Button>
+            <SocialLinks />
+          </div>
+        </div>
       </div>
     </div>
   )
