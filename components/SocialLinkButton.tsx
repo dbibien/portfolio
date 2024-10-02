bcc8e5988aaf9068171a4721e804e66c7b4a1447
Bibi <dbibien@gmail.com>
2024-10-02 02:35:45 -0400

    fix: improved styles

--- components/SocialLinkButton.tsx
+++ components/SocialLinkButton.tsx
@@ -9,7 +9,7 @@ type CProps = {
 const SocialLinkButton = ({ name, path, icon }: CProps) => {
   return (
     <Link href={path} >
-      <button className="bg-zinc-800 hover:bg-zinc-700 transition-all w-32 flex gap-2 justify-center items-center p-2 rounded-xl">
+      <button className="bg-zinc-800 hover:bg-orange-400 transition-all w-32 flex gap-2 justify-center items-center p-2 rounded-xl">
         {icon}
         <span className="text-white/75 hover:text-white transition-all">{name}</span>
       </button>
