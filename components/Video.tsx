1b1f91b1353443ec6eeb2c0e27d7a5ccdb38c4c1
Bibi <dbibien@gmail.com>
2024-10-02 02:24:06 -0400

    fix: styles for mobile

--- components/Video.tsx
+++ components/Video.tsx
@@ -3,7 +3,7 @@ const video = ({ videoId }: { videoId: string }) => {
     <div className="relative">
       <iframe
         // className="absolute top-0 left-0 w-full h-full lg:w-[720px] lg:h-[480px]"
-        className="w-full h-full lg:w-[30rem] lg:h-[20rem]"
+        className="w-full h-[20rem] lg:w-[30rem] lg:h-[20rem]"
         src={`https://www.youtube.com/embed/${videoId}`}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
