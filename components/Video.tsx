3bd139ccc100eb1e4f88f3c8e751c701b6049402
Bibi <dbibien@gmail.com>
2024-10-02 02:18:58 -0400

    set iframe width and height

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,9 +1,9 @@
 const video = ({ videoId }: { videoId: string }) => {
   return (
-    <div className="relative h-[40vh] lg:h-auto">
+    <div className="relative">
       <iframe
         // className="absolute top-0 left-0 w-full h-full lg:w-[720px] lg:h-[480px]"
-        className="w-full h-full lg:w-[600px] lg:h-[400px]"
+        className="w-full h-full lg:w-[30rem] lg:h-[20rem]"
         src={`https://www.youtube.com/embed/${videoId}`}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
