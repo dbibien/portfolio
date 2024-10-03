c99cc6c62bd693749b625a0e7d4641ba76257a4a
Bibi <dbibien@gmail.com>
2024-10-03 15:56:08 -0400

    add: resume title component

---
+++ components/ResumeSectionTitle.tsx
@@ -0,0 +1,5 @@
+const ResumeSectionTitle = ({ title }: { title: string }) => {
+  return <h3 className="mt-4 text-white font-semibold text-lg mb-2">{title}</h3>
+}
+
+export default ResumeSectionTitle 
