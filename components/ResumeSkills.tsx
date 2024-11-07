b1800125fd7c4af71cb7efcd426afefcb404f8d2
Bibi <dbibien@gmail.com>
2024-11-07 15:25:44 -0500

    fix: reformat the the skill section of the resume part of the porfolio website

--- components/ResumeSkills.tsx
+++ components/ResumeSkills.tsx
@@ -1,5 +1,7 @@
 import ResumeSectionTitle from "./ResumeSectionTitle"
+import TechnicalSkills from "./TechnicalSkills"
 import { Separator } from "./ui/separator"
+import { technicalSkills } from "@/lib/utils"
 
 const skillList = ["JavaScript", "TypeScript", "React", "React Native", "Next.js", "Node", "Express", "Jest", "Python",
   "Go", "Django", "Gofiber", "Echo", "HTML", "CSS", "Tailwind", "Nativewind", "Git", "Github", "CI/CD", "AWS/S3", "REST API",
@@ -12,16 +14,22 @@ const ResumeSkills = () => {
       <ResumeSectionTitle title="SKILLS" />
       <Separator />
       <div className="mt-2">
-        {skillList.map((sill, index) => (
-          <span
-            key={index}
-            className="text-white/75"
-          > {sill} |
-          </span>
-        ))}
+        <TechnicalSkills
+          technicalSkills={technicalSkills}
+        />
       </div>
     </div>
   )
 }
 
 export default ResumeSkills
+
+// <div className="mt-2">
+//   {skillList.map((sill, index) => (
+//     <span
+//       key={index}
+//       className="text-white/75"
+//     > {sill} |
+//     </span>
+//   ))}
+// </div>
