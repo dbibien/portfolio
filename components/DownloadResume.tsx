9072f8c24e47def57e0442545986124fac507d11
Bibi <dbibien@gmail.com>
2024-11-07 14:28:19 -0500

    fix: download resume link opens in new tab

--- components/DownloadResume.tsx
+++ components/DownloadResume.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button"
 import { Download } from "lucide-react"
-import Link from "next/link"
 
 const DownloadResume = () => {
   return (
-    <Link href="/assets/bibien_dauphin_resume.pdf">
+    <a
+      target="_blank"
+      href="/assets/bibien_dauphin_resume.pdf"
+      className="hover:bg-orange-400 hover:rounded-xl"
+    >
       <Button
         variant="outline"
         className="text-lg flex items-center gap-2  w-52 h-12"
@@ -12,7 +15,7 @@ const DownloadResume = () => {
         Download Resume
         <Download size="60" />
       </Button>
-    </Link>
+    </a>
 
   )
 }
