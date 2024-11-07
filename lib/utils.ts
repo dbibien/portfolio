7ef9efad1697e20e2bc8e6a0725c53c3c7e1a098
Bibi <dbibien@gmail.com>
2024-11-07 15:27:36 -0500

    Moved list of skill data there

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const technicalSkills = [
+  {
+    name: "Frontend",
+    value: "HTML/CSS/Tailwind, Next.js, React, JavaScript/TypeScript, Zustand, Chadcnui, Material UI (MUI)",
+  },
+  {
+    name: "Mobile",
+    value: "React Native, Expo, Nativewind, JavaScript/TypeScript, Zustand",
+  },
+  {
+    name: "Backend",
+    value: "Node.js, JavaScript/TypeScript, Go, Python, Postgresql, Mysql, Redis",
+  },
+  {
+    name: "Other",
+    value: "HTTP/REST API, Websockets, AWS",
+  },
+]
