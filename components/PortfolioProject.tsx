29ac085ce36a62c2019ab96d4ef386235740dd8d
Bibi <dbibien@gmail.com>
2025-02-04 02:31:43 -0500

    fix: add project title and subtitle

--- components/PortfolioProject.tsx
+++ components/PortfolioProject.tsx
@@ -26,69 +26,74 @@ const data = {
 
 const PortfolioPorject = () => {
   return (
-    <div className="lg:flex lg:flex-row lg:gap-8">
-      <div className="w-full h-full relative">
-        <div className="w-[298px] h-[298px] lg:w-[498px] lg:h-[498px]">
-          <Image
-            src="/assets/portfolioSite.png"
-            alt="Image of portfolio website"
-            priority
-            quality={100}
-            fill
-            className="object-contain"
-          />
-        </div>
-      </div>
+    <div className="mb-8">
+      <h3 className="text-white font-semibold underline text-2xl">{data.title}</h3>
+      <h4 className="text-white/75 mb-4">{data.subTitile}</h4>
 
-      <div className={`lg:order-${data.order.description}`}>
-        <div className="flex justify-center gap-2 mt-4 mb-4 lg:justify-start lg:mt-0 lg:w-[600px]">
-          {data.liveDemoLink && (
-            <SocialLinkButton
-              name="Live Demo"
-              path={data.liveDemoLink}
-              icon={<RadioTower color="white" size={18} />}
+      <div className="lg:flex lg:flex-row lg:gap-8">
+        <div className="w-full h-full relative">
+          <div className="w-[298px] h-[298px] lg:w-[498px] lg:h-[498px]">
+            <Image
+              src="/assets/portfolioSite.png"
+              alt="Image of portfolio website"
+              priority
+              quality={100}
+              fill
+              className="object-contain"
             />
-          )}
-
-          {data.githubLink && (
-            <SocialLinkButton name="Github"
-              path={data.githubLink}
-              icon={<Github color="white" size={18} />}
-            />
-          )}
+          </div>
         </div>
 
-        <div className="text-white/75 text-lg">
-          <h4 className="text-white font-semibold">Description:</h4>
-          <p>{data.description}</p>
+        <div className={`lg:order-${data.order.description}`}>
+          <div className="flex justify-center gap-2 mt-4 mb-4 lg:justify-start lg:mt-0 lg:w-[600px]">
+            {data.liveDemoLink && (
+              <SocialLinkButton
+                name="Live Demo"
+                path={data.liveDemoLink}
+                icon={<RadioTower color="white" size={18} />}
+              />
+            )}
 
-          <h4 className="text-white font-semibold text-md mt-2">Chief benefits of {data.title}:</h4>
-          <ul className="list-inside list-disc">
-            {data.benefits.map((ben, index) => (
-              <li key={index} className="list-item">{ben}</li>
-            ))}
-          </ul>
+            {data.githubLink && (
+              <SocialLinkButton name="Github"
+                path={data.githubLink}
+                icon={<Github color="white" size={18} />}
+              />
+            )}
+          </div>
 
-          <h4 className="text-white font-semibold text-md mt-2">Technology used:</h4>
-          <ul className="list-inside list-disc">
-            <li className="list-item">
-              <span className="font-semibold text-orange-400">Client: </span>{data.tech.frontEnd}
-            </li>
-            <li className="list-item">
-              <span className="font-semibold text-orange-400">Server: </span>{data.tech.backEnd}
-            </li>
-            <li className="list-item">
-              <span className="font-semibold text-orange-400">Other: </span>{data.tech.other}
-            </li>
-          </ul>
+          <div className="text-white/75 text-lg">
+            <h4 className="text-white font-semibold">Description:</h4>
+            <p>{data.description}</p>
 
-          {data.videoId && (
-            <p className="text-white/75 mt-4 text-lg">Take a quick look at the short video demo</p>
-          )}
+            <h4 className="text-white font-semibold text-md mt-2">Chief benefits of {data.title}:</h4>
+            <ul className="list-inside list-disc">
+              {data.benefits.map((ben, index) => (
+                <li key={index} className="list-item">{ben}</li>
+              ))}
+            </ul>
 
-          {data.note && (
-            <p className="text-orange-400/75 mt-4 text-sm">* {data.note}</p>
-          )}
+            <h4 className="text-white font-semibold text-md mt-2">Technology used:</h4>
+            <ul className="list-inside list-disc">
+              <li className="list-item">
+                <span className="font-semibold text-orange-400">Client: </span>{data.tech.frontEnd}
+              </li>
+              <li className="list-item">
+                <span className="font-semibold text-orange-400">Server: </span>{data.tech.backEnd}
+              </li>
+              <li className="list-item">
+                <span className="font-semibold text-orange-400">Other: </span>{data.tech.other}
+              </li>
+            </ul>
+
+            {data.videoId && (
+              <p className="text-white/75 mt-4 text-lg">Take a quick look at the short video demo</p>
+            )}
+
+            {data.note && (
+              <p className="text-orange-400/75 mt-4 text-sm">* {data.note}</p>
+            )}
+          </div>
         </div>
       </div>
     </div>
